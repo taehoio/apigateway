743311536fbfd9a04f9d1768394d4ca6fce3dabd
Taeho Kim <taeho@taeho.io>
2021-05-23 19:20:54 +0900

    Fix setting fields to read only (#6)

--- config/setting.go
+++ config/setting.go
@@ -7,31 +7,65 @@ import (
 )
 
 type Setting struct {
-	ServiceName               string
-	HTTPServerPort            string
-	Env                       string
-	GracefulShutdownTimeoutMs int
-	ShouldProfile             bool
-	ShouldTrace               bool
-
-	ShouldUseGRPCClientTLS          bool
-	CACertFile                      string
-	BaemincryptoGRPCServiceEndpoint string
+	serviceName                     string
+	httpServerPort                  string
+	env                             string
+	gracefulShutdownTimeoutMs       int
+	shouldProfile                   bool
+	shouldTrace                     bool
+	shouldUseGRPCClientTLS          bool
+	caCertFile                      string
+	baemincryptoGRPCServiceEndpoint string
 }
 
 func NewSetting() Setting {
 	return Setting{
-		ServiceName:               "apigateway",
-		HTTPServerPort:            getEnv("HTTP_SERVER_PORT", "8080"),
-		Env:                       getEnv("ENV", "development"),
-		GracefulShutdownTimeoutMs: mustAtoi(getEnv("GRACEFUL_SHUTDOWN_TIMEOUT_MS", "10000")),
-		ShouldProfile:             mustAtob(getEnv("SHOULD_PROFILE", "false")),
-		ShouldTrace:               mustAtob(getEnv("SHOULD_TRACE", "false")),
-
-		ShouldUseGRPCClientTLS:          mustAtob(getEnv("SHOULD_USE_GRPC_CLIENT_TLS", "false")),
-		CACertFile:                      getEnv("CA_CERT_FILE", "/etc/ssl/certs/ca-certificates.crt"),
-		BaemincryptoGRPCServiceEndpoint: getEnv("BAEMINCRYPTO_GRPC_SERVICE_ENDPOINT", "localhost:50051"),
+		serviceName:                     "apigateway",
+		httpServerPort:                  getEnv("HTTP_SERVER_PORT", "8080"),
+		env:                             getEnv("ENV", "development"),
+		gracefulShutdownTimeoutMs:       mustAtoi(getEnv("GRACEFUL_SHUTDOWN_TIMEOUT_MS", "10000")),
+		shouldProfile:                   mustAtob(getEnv("SHOULD_PROFILE", "false")),
+		shouldTrace:                     mustAtob(getEnv("SHOULD_TRACE", "false")),
+		shouldUseGRPCClientTLS:          mustAtob(getEnv("SHOULD_USE_GRPC_CLIENT_TLS", "false")),
+		caCertFile:                      getEnv("CA_CERT_FILE", "/etc/ssl/certs/ca-certificates.crt"),
+		baemincryptoGRPCServiceEndpoint: getEnv("BAEMINCRYPTO_GRPC_SERVICE_ENDPOINT", "localhost:50051"),
 	}
+}
+
+func (s Setting) ServiceName() string {
+	return s.serviceName
+}
+
+func (s Setting) HTTPServerPort() string {
+	return s.httpServerPort
+}
+
+func (s Setting) ENV() string {
+	return s.env
+}
+
+func (s Setting) GracefulShutdownTimeoutMs() int {
+	return s.gracefulShutdownTimeoutMs
+}
+
+func (s Setting) ShouldProfile() bool {
+	return s.shouldProfile
+}
+
+func (s Setting) ShouldTrace() bool {
+	return s.shouldTrace
+}
+
+func (s Setting) ShouldUseGRPCClientTLS() bool {
+	return s.shouldUseGRPCClientTLS
+}
+
+func (s Setting) CACertFile() string {
+	return s.caCertFile
+}
+
+func (s Setting) BaemincryptoGRPCServiceEndpoint() string {
+	return s.baemincryptoGRPCServiceEndpoint
 }
 
 func MockSetting() Setting {
